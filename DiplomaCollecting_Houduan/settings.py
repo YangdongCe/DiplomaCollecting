ff6dc166ee949c7b1f78dbb03096b2c58f6669b5
YangdongCe <yangdongce@imudges.com>
2017-04-14 12:24:10 +0800

    设置了initial，设置了setting链接远程数据库

--- DiplomaCollecting_Houduan/settings.py
+++ DiplomaCollecting_Houduan/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DiplomaCollecting_Houduan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MyExpress',
+        'PASSWORD': '52987632',
+        'USER': 'root',
+        'HOST': '123.206.49.116',
+        'PORT': 3306,
     }
 }
 
